84bc664a57284ac5c3a8a886f4c81810d365a260
federicowoodward <woodfederico@gmail.com>
2025-05-05 18:48:24 -0300

    docker compose agregado usando mysql y migraciones automaticas

--- ApiProductos/ApiProductos/Program.cs
+++ ApiProductos/ApiProductos/Program.cs
@@ -3,33 +3,40 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Agregar contexto de base de datos
+var connection = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("String de conexión no encontrada.");
+
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
-);
+    options.UseMySql(connection, ServerVersion.AutoDetect(connection)));
 
 var app = builder.Build();
 
-// Configurar el pipeline de la aplicación
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<AppDbContext>();
+        context.Database.EnsureDeleted();   
+        context.Database.EnsureCreated();  
+        Console.WriteLine("Base de datos recreada correctamente.");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Ocurrió un error al aplicar migraciones: {ex.Message}");
+    }
+}
+
 app.UseSwagger();
 app.UseSwaggerUI();
 app.UseAuthorization();
 app.MapControllers();
 
-// Ejecutar las migraciones cuando la aplicación arranque
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate(); // Aplica las migraciones automáticamente
-}
-
-// Configurar la URL de la API
 app.Urls.Add("http://0.0.0.0:80");
 
-// Arrancar la aplicación
+// 7. Run
 app.Run();
--- ApiProductos/ApiProductos/obj/Debug/net8.0/ApiProductos.AssemblyInfo.cs
+++ ApiProductos/ApiProductos/obj/Debug/net8.0/ApiProductos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiProductos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6538242e7dff5a8dd0eacd49a4a9f4ff5f1763fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiProductos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiProductos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
