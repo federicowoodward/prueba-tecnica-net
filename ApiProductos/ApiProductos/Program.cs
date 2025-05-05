6538242e7dff5a8dd0eacd49a4a9f4ff5f1763fd
fedewoodward <fwoodward@encodelabs.tech>
2025-05-05 10:31:20 -0300

    intentando agregar docker compose

--- ApiProductos/ApiProductos/Program.cs
+++ ApiProductos/ApiProductos/Program.cs
@@ -4,25 +4,32 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Agregar contexto de base de datos
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+// Configurar el pipeline de la aplicación
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseAuthorization();
-
 app.MapControllers();
 
+// Ejecutar las migraciones cuando la aplicación arranque
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate(); // Aplica las migraciones automáticamente
+}
+
+// Configurar la URL de la API
+app.Urls.Add("http://0.0.0.0:80");
+
+// Arrancar la aplicación
 app.Run();
